1064b8d60c11fc24261693df006244cb5456865c
Yanda Huang <yodaa@umich.edu>
2016-07-04 22:26:20 -0700

    Change to real backend AJAX call

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var ajax = require('ajax');
 
 var CACHE_SIZE = 3;
 var youDaoTranslate = 'http://fanyi.youdao.com/openapi.do?keyfrom=PebbleGreReminder&key=821536062&type=data&doctype=json&only=dict&version=1.1';
+var wordDataBase = 'https://gre-reminder-service.herokuapp.com/word';
 var translateURL = youDaoTranslate;
 
 var cachedWords = [];
@@ -22,10 +23,6 @@ wordCard.add(newWord);
 wordCard.show();
 
 displayNewWord();
-
-function getNewWords(){
-  cachedWords = getMockData(CACHE_SIZE);
-}
 
 var defCard = new UI.Card({
   title: 'Definition',
@@ -61,11 +58,39 @@ wordCard.on('click', 'down', displayNewWord);
 
 function displayNewWord(){
   if (!cachedWords.length){
-    getNewWords();
+    getRealData(CACHE_SIZE);
   }
+  else{
+    console.log("Just update");
+    updateWordView();
+  }
+}
+
+function updateWordView(){
   currentWord = cachedWords[cachedWords.length-1];
   newWord.text(currentWord);
   cachedWords.pop();
+}
+
+function getRealData(amount){
+  ajax(
+  {
+    url: wordDataBase,
+    method: 'get',
+    data: {amount: amount},
+    type: 'json',
+    cache: 'false'
+  },
+  function(data) {
+    // Success!
+    cachedWords = data;
+    updateWordView();
+  },
+  function(error) {
+    // Failure!
+    newWord.text(error.message);
+  }
+);
 }
 
 function getMockData(size){
@@ -74,74 +99,3 @@ function getMockData(size){
   var edge = parseInt(Math.random()*(wordList.length-size));
   return wordList.slice(edge, edge+size);
 }
-
-/*
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
-*/
